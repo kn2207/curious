ccf822e39a0d9caa566a1a52027465c1efe033f0
kn2207 <kn2207@gmail.com>
2024-12-09 16:43:48 -0500

    First cut of snowflake

---
+++ snowflake/1_Snowflake_Basics.sql
@@ -0,0 +1,51 @@
+USE ROLE accountadmin;
+
+USE WAREHOUSE compute_wh;
+
+---> create the Tasty Bytes Database
+CREATE OR REPLACE DATABASE tasty_bytes_sample_data;
+
+---> create the Raw POS (Point-of-Sale) Schema
+CREATE OR REPLACE SCHEMA tasty_bytes_sample_data.raw_pos;
+
+---> create the Raw Menu Table
+CREATE OR REPLACE TABLE tasty_bytes_sample_data.raw_pos.menu
+(
+    menu_id NUMBER(19,0),
+    menu_type_id NUMBER(38,0),
+    menu_type VARCHAR(16777216),
+    truck_brand_name VARCHAR(16777216),
+    menu_item_id NUMBER(38,0),
+    menu_item_name VARCHAR(16777216),
+    item_category VARCHAR(16777216),
+    item_subcategory VARCHAR(16777216),
+    cost_of_goods_usd NUMBER(38,4),
+    sale_price_usd NUMBER(38,4),
+    menu_item_health_metrics_obj VARIANT
+);
+
+---> create the Stage referencing the Blob location and CSV File Format
+CREATE OR REPLACE STAGE tasty_bytes_sample_data.public.blob_stage
+url = 's3://sfquickstarts/tastybytes/'
+file_format = (type = csv);
+
+---> query the Stage to find the Menu CSV file
+LIST @tasty_bytes_sample_data.public.blob_stage/raw_pos/menu/;
+
+---> copy the Menu file into the Menu table
+COPY INTO tasty_bytes_sample_data.raw_pos.menu
+FROM @tasty_bytes_sample_data.public.blob_stage/raw_pos/menu/;
+
+select count(*) from menu;
+
+select * from menu;
+
+--How many items are there with an item_category of 'Snack' and an item_subcategory of 'Warm Option'?
+select count(*) from menu where item_category = 'Snack' and item_subcategory = 'Warm Option';
+
+--What are the max sales prices for each of the three item subcategories (hot option, warm option, cold option)? List from highest price to lowest.
+select item_subcategory,max(sale_price_usd) sale_price_usd
+from menu
+group by 1
+order by 2 desc;
+
