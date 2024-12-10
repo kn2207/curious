d28b8889fc921f00c929fc1c99ad834407805c40
kn2207 <kn2207@gmail.com>
2024-12-10 14:18:34 -0500

    Snowflake learning - Schemas and Tables

---
+++ snowflake/coreInternals/4_Database_And_Schemas.sql
@@ -0,0 +1,43 @@
+SELECT * FROM RAW_POS.MENU;
+
+---> see table metadata
+SELECT * FROM TASTY_BYTES.INFORMATION_SCHEMA.TABLES;
+
+---> create a test database
+CREATE DATABASE test_database;
+
+SHOW DATABASES;
+
+---> drop the database
+DROP DATABASE test_database;
+
+---> undrop the database
+UNDROP DATABASE test_database;
+
+SHOW DATABASES;
+
+create database test_database2;
+
+---> use a particular database
+USE DATABASE test_database;
+
+---> create a schema
+CREATE SCHEMA test_schema;
+
+SHOW SCHEMAS;
+
+SHOW DATABASES;
+
+---> see metadata about your database
+DESCRIBE DATABASE TEST_DATABASE;
+
+---> drop a schema
+DROP SCHEMA test_schema;
+
+SHOW SCHEMAS;
+
+---> undrop a schema
+UNDROP SCHEMA test_schema;
+
+SHOW SCHEMAS;
+
